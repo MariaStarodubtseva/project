fc67f8d5bf3e08d4ad4f118964a9225b58b9d162
Maria Starodubtseva <starodubtseva2@gmail.com>
2021-03-09 23:38:51 +0300

    Init

---
+++ Task2.py
@@ -0,0 +1,28 @@
+a = 10
+print (a)
+
+b = 10.2
+print(b)
+
+c = 'hello'
+print(c)
+
+d = True
+print(d)
+
+e = [10, 10.2, 'hello', True]
+print (e)
+
+f = (10, 10.2, 'hello', True)
+print(f)
+
+g = {'name': 'Мария', 'age': 28}
+print (g)
+
+name = input('Как Вас зовут? ')
+age = input('Сколько Вам лет? ')
+occupation = input('Ваш род занятий? ')
+
+print ('Имя - ', name)
+print ('Возраст - ', age)
+print ('Род занятий -', occupation)
